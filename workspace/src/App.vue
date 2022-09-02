1a0127922e758bbeec3c301a53952c20093b8418
Davidek776 <kocurdavid776@gmail.com>
2022-09-02 18:29:55 +0200

    init.

---
+++ workspace/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <FloorPlan></FloorPlan>
+</template>
+
+<script>
+import FloorPlan from "./components/FloorPlan.vue";
+
+export default {
+  name: "App",
+  components: {
+    FloorPlan,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ workspace/src/components/FloorPlan.vue
@@ -0,0 +1,98 @@
+<template>
+  <body class="bg-red-500">
+    <!-- THE HOUSE - SIMON-->
+
+    <div>
+      <div class="bg-red-500">
+        <!-- <img
+          src="..assets/images/icons/ScrollButton.svg"
+          alt="Scroll Button"
+          class="my-8"
+        /> -->
+      </div>
+
+      <div class="mainHeading flex justify-center">
+        <h1><b>THE</b> HOUSE</h1>
+      </div>
+
+      <div class="flex justify-center floorHeader">
+        <h2><b>FLOOR 1</b></h2>
+      </div>
+      <div class="floorImage flex justify-center">
+        <!--Floor Plan Image-->
+        <img
+          src="../assets/images/FloorPlans/floor_1.png"
+          alt="Floor 1"
+          class="w-4/5 h-auto"
+        />
+
+        <!--Area Images-->
+        <!-- <img
+          src="..assets/images/Shared spaces/kitchen1.JPG"
+          alt="kitchen"
+          class="placePreviewKitchen"
+        />
+        <img
+          src="..assets/images/Shared spaces/dining-area1.JPG"
+          alt="dining area"
+          class="placePreviewDiningArea"
+        /> -->
+      </div>
+
+      <div class="flex justify-center floorHeader">
+        <h2><b>FLOOR 2</b></h2>
+      </div>
+      <div class="floorImage flex justify-center">
+        <!--Floor Plan Image-->
+        <img
+          src="../assets/images/FloorPlans/floor_1.png"
+          alt="Floor 2"
+          class="w-4/5 h-auto"
+        />
+
+        <!--Area Images-->
+        <img
+          src="../assets/images/rooms/room1.jpg"
+          alt="room 1"
+          class="placePreviewRoom1"
+        />
+        <img
+          src="../assets/images/rooms/room1_bathroom.jpg"
+          alt="bathroom 1"
+          class="placePreviewBathroom1"
+        />
+
+        <img
+          src="../assets/images/rooms/room3.jpg"
+          alt="room 3"
+          class="placePreviewRoom3"
+        />
+
+        <img
+          src="../assets/images/rooms/room4.jpg"
+          alt="room 4"
+          class="placePreviewRoom4"
+        />
+        <img
+          src="../assets/images/rooms/room3_4_toilet.jpg"
+          alt="bathroom 3-4"
+          class="placePreviewBathroom34"
+        />
+      </div>
+    </div>
+  </body>
+  <img src="../assets/floorPlanImg/floor_1.png" alt="" />
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+@import "../assets/floorPlanStyles/style.css";
+@import "../assets/floorPlanStyles/output.css";
+</style>
