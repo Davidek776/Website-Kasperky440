0afc184442386ad2e12d3b41305a7ad6b5d06312
Darko Goluza <darkogoluza@gmail.com>
2022-09-04 18:36:17 +0200

    added Reviews!

--- workspace/src/App.vue
+++ workspace/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <FloorPlan></FloorPlan>
+  <Reviews/>
 </template>
 
 <script>
 import FloorPlan from "./components/FloorPlan.vue";
+import Reviews from "@/components/Reviews";
 
 export default {
   name: "App",
   components: {
+    Reviews,
     FloorPlan,
   },
 };
