4419e55e5764bde034578c42974342249a0adc71
xoxkoo <anton.durcak22@gmail.com>
2022-09-07 14:38:18 +0200

    about us section

--- workspace/src/App.vue
+++ workspace/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <FloorPlan></FloorPlan>
+  <AboutUs/>
   <Reviews/>
 </template>
 
 <script>
 import FloorPlan from "./components/FloorPlan.vue";
 import Reviews from "@/components/Reviews";
+import AboutUs from './components/AboutUs.vue'
 
 export default {
   name: "App",
   components: {
     Reviews,
     FloorPlan,
+    AboutUs
   },
 };
 </script>
--- workspace/src/main.js
+++ workspace/src/main.js
@@ -3,3 +3,14 @@ import App from "./App.vue";
 import "./assets/tailwind.css";
 
 createApp(App).mount("#app");
+
+const elements = document.querySelectorAll('.animate')
+document.addEventListener('scroll', () => {
+
+    elements.forEach(element => {
+        if(window.scrollY + window.innerHeight > element.offsetTop + (window.innerHeight * 0.3)) {
+            element.classList.add('animate-element')
+        }
+    })
+    
+})
