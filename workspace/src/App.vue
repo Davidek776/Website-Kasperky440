91b14b8ffa11da1b47aa59a7149d706d2daeb85c
Darko Goluza <darkogoluza@gmail.com>
2022-09-23 01:04:06 +0200

    Made Stats and Contact components

--- workspace/src/App.vue
+++ workspace/src/App.vue
@@ -2,6 +2,9 @@
   <FloorPlan></FloorPlan>
   <AboutUs/>
   <Reviews/>
+  <Stats/>
+  <Contact/>
+  <Footer/>
 </template>
 
 <script>
@@ -9,10 +12,16 @@
 import FloorPlan from "./components/FloorPlan.vue";
 import Reviews from "@/components/Reviews";
 import AboutUs from './components/AboutUs.vue';
+import Contact from "@/components/Contact";
+import Stats from "@/components/Stats";
+import Footer from "@/components/Footer";
 
 export default {
   name: "App",
   components: {
+    Contact,
+    Stats,
+    Footer,
     Reviews,
     FloorPlan,
     AboutUs
---
+++ workspace/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+<footer>footer next, I go to sleep now lol</footer>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
