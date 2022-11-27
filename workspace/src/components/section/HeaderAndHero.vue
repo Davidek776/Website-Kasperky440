daf80c017c6208a9af47452477e37051a9eca920
simonfrayer <315183@via.dk>
2022-11-27 15:46:12 +0100

    dont know about the header and hero David help pls

--- workspace/src/components/section/HeaderAndHero.vue
+++ workspace/src/components/section/HeaderAndHero.vue
@@ -7,82 +7,82 @@
     
     <header>
       <nav
-          class="navigation p-6 2xl:pr-16 bg-transparent mdd:flex mdd:items-center mdd:justify-between"
-          id="logo-parent"
+        class="navigation p-6 2xl:pr-16 bg-transparent mdd:flex mdd:items-center mdd:justify-between ease-in duration-300"
+        id="logo-parent"
       >
         <div class="relative flex justify-between">
-          <h1 class="relative text-3xl font-[Helvetica] cursor-pointer" id="logo">
+          <h1 class="relative text-2xl font-[Helvetica] cursor-pointer" id="logo">
             <img loading="lazy"
-                 src="@/assets/images/icons/logo2Transparent.png"
-                 alt=""
-                 class=" w-10 md:w-20 lg:w-28 xl:w-36"
+              src="@/assets/images/icons/logo2Transparent.png"
+              alt=""
+              class=" w-10 md:w-20 lg:w-28 xl:w-36"
             />
           </h1>
-
+  
           <span
-              class="relative text-3xl md:text-6xl cursor-pointer mx-2 mdd:hidden flex justify-end"
-              ref="bars"
-              @click="Menu(this)"
+            class="relative text-3xl md:text-6xl cursor-pointer mx-2 mdd:hidden flex justify-end"
+            ref="bars"
+            @click="Menu(this)"
           >
-          <!-- <ion-icon name="menu" @click="Menu(this)"></ion-icon> -->
-          <font-awesome-icon icon="fa-solid fa-bars" name="menu" size=""/>
-        </span>
+            <!-- <ion-icon name="menu" @click="Menu(this)"></ion-icon> -->
+            <font-awesome-icon icon="fa-solid fa-bars" name="menu" size="" />
+          </span>
         </div>
-
+  
         <ul
-            class="drop-down mdd:flex mdd:items-center mdd:static absolute bg-white bg-opacity-70 mdd:bg-transparent w-full smm:w-full left-0 mdd:w-auto mdd:py-0 py-4 mdd:pl-0 pl-7 mdd:opacity-100 opacity-0 smm:opacity-100 top-[-400px] ease-in duration-500 z-50 font-[Helvetica]"
-            id="dropId"
+          class="drop-down mdd:flex mdd:items-center mdd:relative mdd:-top-6 absolute bg-white bg-opacity-70 mdd:bg-transparent w-full smm:w-full left-0 mdd:w-auto mdd:py-0 py-4 mdd:pl-0 pl-7 mdd:opacity-100 opacity-0 smm:opacity-100 top-[-400px] transition-all ease-in duration-500 z-50 font-[Helvetica]"
+          id="dropId"
         >
-          <li class="mx-4 my-6 mdd:my-0 ">
+          <li class="mx-4 my-6 mdd:my-0 transition-all ease-in duration-300">
             <a
-                href="#"
-                @click="gotoHome()"
-                class=" mdd:hover:text-cyan-500 duration-500"
-            >Domů</a
+              href="#"
+              @click="gotoHome()"
+              class="text-sm 3xl:text-3xl xl:text-xl mdd:hover:text-cyan-500 duration-500"
+              >Domů</a
             >
           </li>
-          <!-- hover:color-stone-50  -->
-          <li class="mx-4 my-6 mdd:my-0 ">
+          <!-- hover:color-stone-50 transition-all ease-in duration-300 -->
+          <li class="mx-4 my-6 mdd:my-0 transition-all ease-in duration-300">
             <a
-                href="#"
-                @click="gotoAccomodation()"
-                class=" mdd:hover:text-cyan-500 duration-500"
-            >Ubytování</a
+              href="#"
+              @click="gotoAccomodation()"
+              class="text-sm 3xl:text-3xl xl:text-xl mdd:hover:text-cyan-500 duration-500"
+              >Ubytování</a
             >
           </li>
-          <li class="mx-4 my-6 mdd:my-0 ">
+          <li class="mx-4 my-6 mdd:my-0 transition-all ease-in duration-300">
             <a
-                href="#"
-                @click="gotoAbout()"
-                class=" mdd:hover:text-cyan-500 duration-500"
-            >O Nás</a
+              href="#"
+              @click="gotoAbout()"
+              class="text-sm 3xl:text-3xl xl:text-xl mdd:hover:text-cyan-500 duration-500 "
+              >O Nás</a
             >
           </li>
-          <li class="mx-4 my-6 mdd:my-0 ">
+          <li class="mx-4 my-6 mdd:my-0 transition-all ease-in duration-300">
             <a
-                href="#"
-                @click="gotoReview()"
-                class="mdd:hover:text-cyan-500 duration-500"
-            >Recenze</a
+              href="#"
+              @click="gotoReview()"
+              class=" text-sm 3xl:text-3xl xl:text-xl mdd:hover:text-cyan-500 duration-500"
+              >Recenze</a
             >
           </li>
           <li
-              class="mx-4 mr-0 my-6 mdd:my-0 mb-8 "
+            class="mx-4 mr-0 my-6 mdd:my-0 mb-8 transition-all ease-in duration-300"
           >
             <a
-                href="#"
-                @click="gotoContact()"
-                class=" mdd:hover:text-cyan-500 duration-500"
-            >Kontakt</a
+              href="#"
+              @click="gotoContact()"
+              class="text-sm 3xl:text-3xl xl:text-xl mdd:hover:text-cyan-500 duration-500 "
+              >Kontakt</a
             >
           </li>
           <a
-              href="https://www.booking.com/hotel/cz/kasperky-440.cs.html"
-              target="_blank"
+            href="https://www.booking.com/hotel/cz/kasperky-440.cs.html"
+            target="_blank"
           >
             <button
-                class="bg-slate-800 text-white font-[Helvetica] duration-500 px-6 py-2 mx-8 hover:bg-slate-500 rounded-3xl"
-                id="reserveBtn"
+              class="bg-slate-800 text-white font-[Helvetica] duration-500 px-6 py-2 mx-8 hover:bg-slate-500 rounded-3xl text-sm 3xl:text-3xl xl:text-xl"
+              id="reserveBtn"
             >
               Rezervovat
             </button>
@@ -96,7 +96,7 @@
               <img loading="lazy"
                    src="@/assets/images/icons/czech-republic.png"
                    alt=""
-                   class="md:w-10 w-16"
+                   class="w-8 xl:w-10 3xl:w-14"
               />
             </button>
             <button class="duration-300"
@@ -106,7 +106,7 @@
               <img
                   src="@/assets/images/icons/united-kingdom.png"
                   alt=""
-                  class="w-8"
+                  class="w-8 xl:w-10 3xl:w-14"
               />
             </button>
           </ul>
@@ -115,13 +115,14 @@
       </nav>
 
       <div class="hero_content">
-        <div class="motto">
-          <h1 class="quote">
-            „<span> Pohodové Ubytování Pro Vaši Relaxaci </span>“
+        <div class="motto hidden mdd:relative top-0 3xl:-top-10">
+          <h1 class="quote text-4xl xl:text-5xl 3xl:text-6xl">
+            „<span class=""> Pohodové Ubytování Pro Vaši Relaxaci </span>“
           </h1>
         </div>
       </div>
     </header>
+
     <div class="pre-content">
       <div class="description">
         <div class="left animate animate-l">
@@ -181,7 +182,7 @@ export default {
       let list = document.querySelector("ul");
       let nav = document.querySelector("nav");
       // let quote = document.querySelector(".quote");
-      list.classList.remove("top-[90px]");
+      list.classList.remove("top-[110px]");
       list.classList.remove("opacity-100");
       // document.getElementsByClassName("bars").dispatchEvent(new Event("click"));
       // this.eventFire(document.getElementsByClassName("bars"), "click");
@@ -189,23 +190,21 @@ export default {
         this.$refs["bars"].click();
         this.clicks++;
       }
-
       e.name === "menu"
-          ? ((e.name = "close"),
-              list.classList.add("top-[90px]"),
-              list.classList.add("opacity-100"),
-              nav.classList.remove("bg-transparent"),
-              nav.classList.add("bg-white"),
-              console.log(list.classList),
-              this.controlTxt())
-          : // nav.classList.add("bg-opacity-80")
+        ? ((e.name = "close"),
+          list.classList.add("top-[110px]"),
+          list.classList.add("opacity-100"),
+          nav.classList.remove("bg-transparent"),
+          nav.classList.add("bg-white"),
+          console.log(list.classList),
+          this.controlTxt())
+        : // nav.classList.add("bg-opacity-80")
           ((e.name = "menu"),
-              list.classList.remove("top-[110px]"),
-              list.classList.remove("opacity-100"),
-              nav.classList.add("bg-transparent"),
-              nav.classList.remove("bg-white"),
-              this.controlTxt());
-
+          list.classList.remove("top-[110px]"),
+          list.classList.remove("opacity-100"),
+          nav.classList.add("bg-transparent"),
+          nav.classList.remove("bg-white"),
+          this.controlTxt());
       console.log(nav.classList);
     },
     myEventHandler(e) {
